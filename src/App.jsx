7a9c7fd8cfaee5acbe82016c2d4b7c6465fe3ec5
Ashraf <mohammadashrafmohammadi514@gmail.com>
2024-08-10 11:34:33 +0430

    setup react router

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,32 @@ import "@mantine/core/styles.css";
 import { Container } from "@mantine/core";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import TodoPage from "./pages/todo.page";
-// import {
-//   createBrowserRouter,
-//   RouterProvider,
-// } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 const queryClient = new QueryClient();
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <div>Hello world!</div>,
-//   },
-//   {
-//     path: "todo",
-//     element: <TodoPage/>,
-//   },
-// ]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "todo",
+    element: <TodoPage/>,
+  },
+]);
 
 function App() {
   return (
     // <MantineProvider>
     <QueryClientProvider client={queryClient}>
       <Container>
-      {/* <RouterProvider router={router} > */}
+      <RouterProvider router={router} >
         <TodoPage />
-    {/* </RouterProvider> */}
+    </RouterProvider>
       </Container>
     </QueryClientProvider>
     // </MantineProvider>
